775501cf66b9fca7f6ca08c718368c2184dd5f52
Luissantos20 <luissilvassantos@gmail.com>
2025-03-17 11:21:06 -0300

    Add files via upload

---
+++ Conceitos-Fundamentais/Alocação-Dinâmica/exemplo_alocacao_Din.py
@@ -0,0 +1,16 @@
+# Exemplo de Alocação Dinâmica com Listas em Python
+
+# Em Python, as listas são alocadas dinamicamente, ou seja, podemos adicionar ou remover elementos a qualquer momento.
+
+# Inicializando uma lista vazia
+lista = []
+
+# Adicionando elementos dinamicamente
+for i in range(10):
+    lista.append(i)  # Alocação dinâmica: o tamanho da lista pode crescer durante a execução
+
+# Imprimindo os valores
+print("Lista de alocação dinâmica:", lista)
+
+
+
