9a82e49c7e711d6de662d6df58f6728f45d23535
Luissantos20 <luissilvassantos@gmail.com>
2025-03-17 11:30:17 -0300

    Add files via upload

---
+++ Estruturas-Lineares/Listas/listas.py
@@ -0,0 +1,22 @@
+# Listas em Python
+
+# Criando uma lista
+lista = [1, 2, 3, 4, 5]
+
+# Inserindo um item no final
+lista.append(6)  # [1, 2, 3, 4, 5, 6]
+
+# Removendo um item
+lista.remove(3)  # [1, 2, 4, 5, 6]
+
+# Acessando um item
+print("Elemento na posição 2:", lista[2])  # Saída: 4
+
+# Inserindo um item no meio
+lista.insert(2, 10)  # [1, 2, 10, 4, 5, 6]
+
+# Verificando o tamanho da lista
+print("Tamanho da lista:", len(lista))  # Saída: 6
+
+# Exibindo a lista final
+print("Lista final:", lista)
